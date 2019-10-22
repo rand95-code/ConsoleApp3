04d02b7aadfb3209075f33bee969d30cc1867939
Rand Farhan <rand.farhan@gmail.com>
2019-10-22 16:04:30 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Interations
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+                for (int i = 10; i >= 1; i--)
+            {
+                {
+                    if (i % 2 == 0)
+                    {
+
+                        Console.WriteLine(i);
+                    }
+                }
+
+
+            }
+        }
+    }
+}
